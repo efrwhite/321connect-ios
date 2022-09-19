ddfaf55abd09324d100e7bdc06546ea3a8762e56
Boston <Shippotheboss3@gmail.com>
2022-09-19 17:12:39 -0400

    Sept 19th
    This is my published branch, Sep19

--- 321connect-ios/321connect-ios/AppDelegate.swift
+++ 321connect-ios/321connect-ios/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Documents Directory: ", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found!")
         // Override point for customization after application launch.
         
 //        if #available(iOS 15.0, *) {
@@ -38,6 +41,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    func applicationWillTerminate(_ application: UIApplication) {
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    // uses memory when needed is considered a lazy variable, this is a sqlite database
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "321ConnectDatabase")  //must match our datamodel name
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    // saves data when application gets terminated
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 
--- 321connect-ios/321connect-ios/MessageView.swift
+++ 321connect-ios/321connect-ios/MessageView.swift
@@ -5,18 +5,53 @@
 //  Created by Bri Boston on 6/23/22.
 //
 
-import Foundation
+
+import CoreData
 import UIKit
-class MessageViewController:UIViewController {
+
+class MessageViewController: UIViewController {
     @IBOutlet weak var NotesField: UITextView!
+    var messageArray = [Message]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     @IBAction func SaveButton(_ sender: UIButton) {
-        let notes = NotesField.text
-        print("Notes:\(notes)")
+        
+            let new_message = Message(context: self.context)
+            new_message.message_Block = NotesField.text
+            
+            self.messageArray.append(new_message)
+            
+            
+            self.SaveItems()
+        
+    
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        print("LINK: ")
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadItems()
+       
     }
+    func SaveItems(){
+       
+        do {
+            try context.save()
+        } catch {
+            print("Error Saving context \(error)")
+        }
+        
+        
+    }
+
+    func loadItems(){
+        let request : NSFetchRequest<Message> = Message.fetchRequest()
+        do{
+        messageArray = try context.fetch(request)
+        } catch{
+            print("Error fetching data \(error)")
+        }
+    }
+    
 }
