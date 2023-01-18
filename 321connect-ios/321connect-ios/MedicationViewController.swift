ed592f27d20584545a9b4373a4467c580118019c
Elizabeth White Baker <efrwhite@aol.com>
2023-01-18 17:43:23 -0500

    Revert "pushingSpringBoston"

--- 321connect-ios/321connect-ios/MedicationViewController.swift
+++ 321connect-ios/321connect-ios/MedicationViewController.swift
@@ -3,61 +3,27 @@
 //  321connect-ios
 //
 //  Created by Edward Ladia on 8/2/22.
-//  Modified By Brianna Boston 1/14/23
+//
 
 import UIKit
 
-class MedicationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, DataEnteredDelegate {
-    
-    
-  
-    var medicationlist:[String] = ["Advil"]
-    var medication = ""
-    @IBOutlet weak var MedicationList: UITableView!
-    @IBOutlet weak var labelMeds: UILabel!
-    
-     override func viewDidLoad() {
+class MedicationViewController: UIViewController {
+
+    override func viewDidLoad() {
         super.viewDidLoad()
-        MedicationList.delegate = self
-        MedicationList.dataSource = self
-        labelMeds.text = "Medication Added:\(medication)"
-        medicationlist.append(medication)
 
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "MedicationAddition" {
-                let secondViewController = segue.destination as! MedicationAddition
-                secondViewController.delegate = self
-            }
-        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
-        func userDidEnterInformation(info: String) {
-            labelMeds.text = info
-            print("This is my label:",labelMeds.text)
-        }
-    
-    
-    
-    
-    
- // Tapped Button
-    @IBAction func add_Medication(_ sender: Any) {
-    }
-   
-    //TABLE VIEW CODE
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return medicationlist.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = medicationlist[indexPath.row]
-        return cell
-    }
-    func loadData() {
-        // code to load data from network, and refresh the interface
-        MedicationList.reloadData()
-    }
-    
 }
