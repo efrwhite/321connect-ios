e39f28081ca64ce406e6a08698435e28d7e07fd4
Boston <Shippotheboss3@gmail.com>
2023-02-07 11:34:01 -0500

    Feb 7 PUsh

--- 321connect-ios/321connect-ios/Profile Types/ParentCaregiverViewController.swift
+++ 321connect-ios/321connect-ios/Profile Types/ParentCaregiverViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import CoreData
 
 class Parent_Caregiver_ViewController: UIViewController, UITextFieldDelegate {
-    
+    var StringArray = [String]()
     var isFirstTimeSignUp = false
     @IBOutlet weak var ParentPicture: UIImageView!
     @IBOutlet weak var FirstName: UITextField!
@@ -30,7 +30,10 @@ class Parent_Caregiver_ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         receivedString = user
+        
         print("Parent Passed:", receivedString)
+        loadItems()
+        print("String Array ", StringArray)
         // profile image mask and style
         ParentPicture.layer.borderWidth = 1.0
         ParentPicture.layer.masksToBounds = false
@@ -38,9 +41,11 @@ class Parent_Caregiver_ViewController: UIViewController, UITextFieldDelegate {
         ParentPicture.layer.cornerRadius = ParentPicture.frame.size.width/2
         ParentPicture.clipsToBounds = true
         
+       
         // Gesture to collapse/dismiss keyboard on click outside
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -62,6 +67,15 @@ class Parent_Caregiver_ViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    override func viewWillAppear(_ animated: Bool) {
+        FirstName.text = StringArray[0]
+        LastName.text = StringArray[1]
+        FirstName.placeholder = StringArray[0]
+        LastName.placeholder = StringArray[1]
+        PhoneNumber.placeholder = StringArray[2]
+        Username.placeholder = receivedString
+        Password.placeholder = StringArray[3]
+    }
     
     // dismiss Keyboard
     @objc func dismissKeyboard() {
@@ -71,7 +85,7 @@ class Parent_Caregiver_ViewController: UIViewController, UITextFieldDelegate {
         if (segue.identifier == "parentToChild")
         {
             let displayVC = segue.destination as! ChildView
-            //            displayVC.user = usernameTextField.text!
+            displayVC.user = receivedString
             displayVC.isFirstTimeSignUp = true
         }
     }
@@ -80,6 +94,11 @@ class Parent_Caregiver_ViewController: UIViewController, UITextFieldDelegate {
         
         // first time sign up -> show child VC
         if isFirstTimeSignUp {
+            viewWillAppear(true)
+            FirstName.text = StringArray[0]
+            LastName.text = StringArray[1]
+            PhoneNumber.text = StringArray[2]
+            Password.text = StringArray[3]
             let new_parent = Parent(context: self.context)
             new_parent.firstName = FirstName.text
             new_parent.lastName = LastName.text
@@ -152,8 +171,22 @@ extension Parent_Caregiver_ViewController: UIImagePickerControllerDelegate, UINa
 
     func loadItems(){
         let request : NSFetchRequest<Parent> = Parent.fetchRequest()
+        let account_request: NSFetchRequest<Account> = Account.fetchRequest()
+        account_request.predicate = NSPredicate(format:"userName = %@", receivedString)
+        account_request.fetchLimit = 1
+        let link = (try? context.fetch(account_request))!
+        for re in link {
+            StringArray.append(re.firstName!)
+            StringArray.append(re.lastName!)
+            StringArray.append(re.phone!)
+            StringArray.append(re.passWord!)
+        
+        }
+        
+        
         do{
             ParentArray = try context.fetch(request)
+            
         } catch{
             print("Error fetching data \(error)")
         }
